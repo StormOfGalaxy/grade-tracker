7549daaa6b279248b36bfa95e4e190c949145cd5
paul <paul.landgraf009@gmail.com>
2025-03-04 00:08:58 +0100

    [ADD] added user authentification with supabase

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+const emit = defineEmits(["login"]);
+
+const handleSubmit = () => {
+  emit("login", { email: email.value, password: password.value });
+};
+</script>
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle class="text-2xl"> Login </CardTitle>
+      <CardDescription>
+        Enter your email below to login to your account
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <form @submit.prevent="handleSubmit">
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              v-model="email"
+              type="email"
+              placeholder="m@example.com"
+              autocomplete="username email"
+              required
+            />
+          </div>
+          <div class="grid gap-2">
+            <Label for="password">Password</Label>
+            <Input
+              id="password"
+              v-model="password"
+              type="password"
+              autocomplete="current-password"
+              required
+            />
+          </div>
+          <Button type="submit" class="w-full"> Login </Button>
+        </div>
+      </form>
+    </CardContent>
+  </Card>
+</template>
